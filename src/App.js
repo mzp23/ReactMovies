812fb116675ec921c9864c9df32b0648d36b741a
Vitaliy Sliusar <mzgme23@gmail.com>
2020-02-19 02:58:57 +0200

    HW#28-REACT: add buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-
+// import './App.module.scss'
+import Button from "./components/Button/Button";
+import styles from './App.module.scss'
 function App() {
   return (
-    <div>
-      'Init homework'
-    </div>
+      <section className={styles.sorting}>
+        <h2>Sort movies</h2>
+          <div>
+              <Button title="by likes"/>
+              <Button title="by rating"/>
+              <Button title="reset"/>
+          </div>
+
+      </section>
+
   );
 }
 
---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Button.module.scss';
+const Button = ({title}) => {
+    return (
+        <button className={styles.button}>{title}</button>
+    );
+};
+
+export default Button;
