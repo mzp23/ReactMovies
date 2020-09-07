f7b91a77793d8952a70a7a6ec295b3f9c75e0225
mzp23 <mzgme23@gmail.com>
2020-09-07 16:41:03 +0300

    refactor star component

--- src/components/Star/component.jsx
+++ src/components/Star/component.jsx
@@ -1,22 +1,22 @@
 import React from "react";
-import FilledStar from "../FilledStar/componetn";
-import EmptyStar from "../EmptyStar/componetn";
+import uuid from "uuid";
 
-const MyComponent = props => {
-  const { handleStar, movieId, star, isFilled} = props;
-  return isFilled ? (
-    <FilledStar
-      handleStar={handleStar}
-      movieId={movieId}
-      star={star}
-    />
-  ) : (
-    <EmptyStar
-      handleStar={handleStar}
-      movieId={movieId}
-      star={star}
-    />
+import styles from './style.module.scss';
+
+const Star = ({ handleStar, movieId, star, isFilled}) => {
+  const starToRender = isFilled ? '⭐' : '☆';
+
+  return (
+    <span
+      className={styles.stars}
+      onClick={() => handleStar(movieId, star)}
+      key={uuid()}
+      role="img"
+      aria-label="gold star"
+    >
+      {starToRender}
+    </span>
   );
 };
 
-export default MyComponent;
+export default Star;
